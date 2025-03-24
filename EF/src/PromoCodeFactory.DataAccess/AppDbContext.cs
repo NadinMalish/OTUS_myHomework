20a9d0274f280f085beae1bf5c78ca04243a8081
NadinMalish <lazarevanv@mail.ru>
2025-03-24 13:03:27 +0300

    EF - 2_my_Homework

---
+++ EF/src/PromoCodeFactory.DataAccess/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PromoCodeFactory.Core.Domain.Administration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromoCodeFactory.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Работники.
+        /// </summary>
+        public DbSet<Employee> Employees { get; set; }
+
+        /// <summary>
+        /// Роли.
+        /// </summary>
+        public DbSet<Role> Roles { get; set; }
+
+
+    }
+}
---
+++ EF/src/PromoCodeFactory.DataAccess/Repositories/Repository.cs
@@ -0,0 +1,39 @@
+﻿using PromoCodeFactory.Core.Domain;
+using PromoCodeFactory.Core.Abstractions.Repositories;
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PromoCodeFactory.DataAccess.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        protected readonly AppDbContext _db;
+        private readonly DbSet<T> _entitySet;
+
+        protected IEnumerable<T> Data { get; set; }
+
+        public Repository(AppDbContext db)
+        {
+            _db = db;
+            _entitySet = _db.Set<T>();
+        }
+
+
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _entitySet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _entitySet.FirstOrDefaultAsync(id);
+        }
+    }
+}
